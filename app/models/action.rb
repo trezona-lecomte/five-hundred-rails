09388d05d1024ff8daa0b94e06f8b9c4709a3f32
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-07-28 10:39:08 +1200

    Delete actions resource

--- app/models/action.rb
+++
@@ -1,5 +0,0 @@
-class Action < ActiveRecord::Base
-  belongs_to :round
-  belongs_to :card
-  belongs_to :player
-end
--- app/serializers/action_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ActionSerializer < ActiveModel::Serializer
-  attributes :id, :round_id, :player_id, :card_id
-end
