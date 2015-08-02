47876befeb11a9f4c53f841caeaad3e136b9cc39
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-08-02 21:49:31 +1200

    Add specs for games controller

---
+++ spec/requests/games_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+RSpec.describe "Games", :type => :request do
+  before { 3.times { Game.create! } }
+
+  describe "GET /games" do
+    before { get "/games", format: :json }
+
+    it "returns with 200 OK" do
+      expect(response).to have_http_status(200)
+    end
+
+
+    it "returns a list of all the games" do
+      expect(response.body).to include("games")
+      expect(response.body).to include("1")
+      expect(response.body).to include("2")
+      expect(response.body).to include("3")
+    end
+  end
+
+  describe "GET /games/:id" do
+    before { get "/games/1", format: :json }
+
+    it "returnw with 200 OK" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the requested game" do
+      expect(response.body).to include("1")
+    end
+  end
+
+  describe "POST /games" do
+    before { post "/games", game: {} }
+
+    it "responds with 201 Created" do
+      expect(response).to have_http_status(201)
+    end
+
+    it "creates a game" do
+      expect { post "/games", game: {} }.to change(Game, :count).by(1)
+    end
+  end
+
+  describe "PUT /games/:id" do
+    before do
+      game = Game.create!
+      4.times { JoinGame.new(game, Faker::Internet.user_name).call }
+      start_round = StartRound.new(game)
+      start_round.call
+      DealCards.new(game, start_round.round).call
+
+      put "/games/1", { round_id: 1, player_id: 1, card_id: 1 }
+    end
+
+    it "updates the game with the card specified" do
+      expect(response).to have_http_status(200)
+    end
+  end
+end
---
+++ spec/services/start_round_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe StartRound, type: :service do
+  describe "#call" do
+    context "when the round can be started" do
+
+    end
+  end
+end
