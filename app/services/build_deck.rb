73f8df3f6fefb35b2a9ceeabd1bd85ebb9a6ec44
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-08-03 13:47:32 +1200

    Create build_deck service

---
+++ app/services/build_deck.rb
@@ -0,0 +1,25 @@
+class BuildDeck
+  attr_reader :deck
+
+  def initialize
+    @deck = []
+  end
+
+  def call
+    standard_deck.shuffle!
+  end
+
+  private
+
+  def standard_deck
+    Card.ranks.select { |_, val| val.between?(5, 14) }.keys
+      .product(Card.suits.select { |suit, _| suit != "no_suit" }.keys)
+        .each do |rank, suit|
+          @deck << Card.new(rank: rank, suit: suit)
+      end
+
+    @deck << Card.new(rank: "4", suit: "spades")
+    @deck << Card.new(rank: "4", suit: "clubs")
+    @deck << Card.new(rank: "joker", suit: "no_suit")
+  end
+end
---
+++ spec/services/build_deck_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe BuildDeck, type: :service do
+  describe "#call" do
+    let(:build_deck) { BuildDeck.new }
+
+    context "when the deck is successfully built" do
+      it "returns a standard-size deck of cards" do
+        expect(build_deck.call.size).to eq(43)
+      end
+    end
+  end
+end
