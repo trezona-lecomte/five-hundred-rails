019eadbd9c096e3e96b92b0745aced3a754322cd
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-08-03 13:48:02 +1200

    Add 0-3 to cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ActiveRecord::Base
   belongs_to :round
   belongs_to :trick
 
-  enum rank: %w(4 5 6 7 8 9 10 jack queen king ace joker)
+  enum rank: %w(0 1 2 3 4 5 6 7 8 9 10 jack queen king ace joker)
   enum suit: %i(spades clubs diamonds hearts no_suit)
 
   validates :round, presence: true
