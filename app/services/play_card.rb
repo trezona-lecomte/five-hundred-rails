5909e08a94a398b90d53abc8e07ceda53b8cee44
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-05 08:49:42 +1200

    Add beginnings of core card-playing functionality

--- app/services/play_card.rb
+++ app/services/play_card.rb
@@ -10,7 +10,9 @@ class PlayCard
 
   def call
     @card.with_lock do
-      play_card if card_can_be_played
+      validate_card_can_be_played
+
+      play_card unless errors.present?
     end
 
     success?
@@ -18,24 +20,35 @@ class PlayCard
 
   private
 
-  def card_can_be_played
-    if players_turn? && card_not_already_played? && player_owns_card?
-      true
-    else
-      add_error("you can't play this card right now")
-      false
+  def validate_card_can_be_played
+    unless players_turn?
+      add_error("it's not your turn to play")
+    end
+
+    if card_already_played?
+      add_error("you have already played this card")
+    end
+
+    unless player_owns_card?
+      add_error("you don't have this card in your hand")
     end
   end
 
   def players_turn?
     # if it's trick 1, then the winning bidder starts
+    # TODO: for now we assume that player 2 won the bidding:
+    if round.tricks.count == 1 && round.tricks.last.cards.empty?
+      @player == @round.game.players.first(2).last
+    else
+      # if it's trick 2.. then the winner of the last trick starts
+      trick = TricksDecorator.new(round.tricks.last)
 
-    # if it's trick 2.. then the winner of the last trick starts
-    true
+      @player == trick.winning_card.player
+    end
   end
 
-  def card_not_already_played?
-    @card.trick.blank?
+  def card_already_played?
+    @card.trick.present?
   end
 
   def player_owns_card?
@@ -45,7 +58,7 @@ class PlayCard
   def play_card
     trick = @round.tricks.last
 
-    if trick.cards.count > 3
+    unless trick && trick.cards.count < 4
       trick = trick.round.tricks.create!
     end
 
@@ -56,11 +69,11 @@ class PlayCard
     end
   end
 
-  def success?
-    @errors.empty?
-  end
-
   def add_error(message)
     @errors << message
+  end
+
+  def success?
+    @errors.empty?
   end
 end
--- spec/services/play_card_spec.rb
+++ spec/services/play_card_spec.rb
@@ -1,53 +1,69 @@
 require "rails_helper"
 
 RSpec.describe PlayCard, type: :service do
-  let(:game)  { Game.create! }
-  let(:round) { game.rounds.first }
-  let(:player) { game.players.first }
-  let(:card)   { player.cards.first }
-
-  before do
-    4.times { JoinGame.new(game, Faker::Internet.user_name).call }
-    start_round = StartRound.new(game)
-    start_round.call
-    round = start_round.round
-    DealCards.new(game, round, BuildDeck.new.call).call
-  end
+  fixtures :all
+  let(:game)  { games(:playing_game) }
+  let(:round) { rounds(:playing_round) }
+  # TODO: we're assuming (for now) that player 2 won the bidding:
+  let(:player) { players(:player2) }
+  let(:card)   { cards(:jack_of_hearts) }
 
   describe "#call" do
     before { PlayCard.new(round, player, card).call }
 
-    context "when it is the first round" do
-      context "when it is the first trick" do
-        context "when player 1 attempts to play a card" do
-          let(:player) { game.players.first }
-
-          it "adds the card to the trick" do
-            expect(card.trick).to eq(round.tricks.first)
-          end
-        end
-
-        context "when player 2 attempts to play a card" do
-          let(:player) { game.players.first(2).last }
-
-          it "doesn't add the card to the trick" do
-            expect(card.trick).to be_nil
-          end
-
+    context "when it is the first trick" do
+      context "when the bid-winner attempts to play a card" do
+        it "plays the card" do
+          expect(card.trick).to eq(round.tricks.first)
         end
       end
 
-      context "when it isn't the first trick" do
+      context "when a non-winning bidder attempts to play a card" do
+        let(:player) { players(:player1) }
+        let(:card)   { cards(:king_of_hearts) }
 
+        it "doesn't play the card" do
+          expect(card.trick).to be_nil
+        end
+        # TODO validate error message
       end
     end
 
-    context "when it isn't the first round" do
+    context "when it isn't the first trick" do
+      before do
+        PlayCard.new(round, players(:player2), cards(:ten_of_hearts)).call
+        PlayCard.new(round, players(:player3), cards(:nine_of_hearts)).call
+        PlayCard.new(round, players(:player4), cards(:six_of_hearts)).call
+        PlayCard.new(round, players(:player1), cards(:king_of_hearts)).call
+      end
 
+      context "when the last trick winner attempts to play a card" do
+        let(:player) { players(:player1) }
+        let(:card)   { cards(:king_of_diamonds) }
+
+        before { PlayCard.new(round, player, card).call }
+
+        it "plays the card" do
+          expect(card.trick).to eq(round.tricks.last)
+        end
+      end
+
+      context "when anyone but the last trick winner attempts to play a card" do
+        let(:player)    { players(:player2) }
+        let(:card)      { cards(:jack_of_hearts)}
+        let(:play_card) { PlayCard.new(round, player, card) }
+
+        before          { play_card.call }
+
+        it "doesn't play the card" do
+          expect(card.trick).to be_nil
+        end
+
+        # TODO validate error message
+        it "sets the error to 'not your turn'" do
+          expect(play_card.errors).to include("it's not your turn to play")
+        end
+      end
     end
-  end
-
-  context "when the card can't be played" do
-
   end
 end
