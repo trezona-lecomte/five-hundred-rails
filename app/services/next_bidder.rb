c3187f37a17c854cf3d62e4a4728f75eaa5b94df
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-06 10:50:12 +1200

    Handle multiple passes in next_bidder

--- app/services/next_bidder.rb
+++ app/services/next_bidder.rb
@@ -10,20 +10,45 @@ class NextBidder
 
   def call
     @round.with_lock do
-
-      passed_players_offset = 0
-
-      until (@next_bidder && @round.passes.none? { |pass| pass.player == @next_bidder }) || passed_players_offset > 3
-        next_player_index = (@round.bids.count % @players.count) + passed_players_offset
-
-        if next_player_index < @players.length
-          @next_bidder = @players[next_player_index]
-        else
-          @next_bidder = @players[0]
-        end
-
-        passed_players_offset += 1
-      end
+      set_next_bidder
     end
+
+    unless @next_bidder
+      add_error("bidding is over")
+    end
+  end
+
+  private
+
+  def set_next_bidder
+    passed_players_offset = 0
+
+    until @next_bidder || passed_players_offset > 3
+      incremented_index = next_bidder_index(passed_players_offset)
+
+      @next_bidder = fetch_next_bidder(incremented_index)
+
+      if bidder_has_already_passed?
+        @next_bidder = nil
+      end
+
+      passed_players_offset += 1
+    end
+  end
+
+  def bidder_has_already_passed?
+    @round.passes.any? { |pass| pass.player == @next_bidder }
+  end
+
+  def next_bidder_index(offset)
+    (@round.bids.count % @players.count) + offset
+  end
+
+  def fetch_next_bidder(index)
+    index < @players.length ? @players[index] : @players[0]
+  end
+
+  def add_error(message)
+    @errors << message
   end
 end
--- spec/services/next_bidder_spec.rb
+++ spec/services/next_bidder_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe NextBidder, type: :service do
   fixtures :all
   let(:game)             { games(:bidding_game) }
-  #let(:round)            { rounds(:bidding_round) }
   let(:find_next_bidder) { NextBidder.new(round) }
 
   describe "#call" do
@@ -13,7 +12,7 @@ RSpec.describe NextBidder, type: :service do
           let(:round) { rounds(:bidding_round) }
 
           before do
-            submit_bids(n)
+            submit_bids(round, n)
             find_next_bidder.call
           end
 
@@ -25,33 +24,94 @@ RSpec.describe NextBidder, type: :service do
     end
 
     context "when there are passes" do
-      before { submit_passes(1) }
+      3.times do |n|
+        context "when it is the first round" do
+          let(:round) { rounds(:bidding_round) }
 
-      it "returns the correct bidder" do
+          before do
+            submit_passes(round, n)
+            find_next_bidder.call
+          end
 
+          it "returns the correct bidder" do
+            expect(find_next_bidder.next_bidder).to eq(players("bidder#{(n % game.players.count) + 1}"))
+          end
+        end
       end
     end
 
-    context "when it is the fourth round" do
+    context "when there is a bid and a pass" do
+      context "when it is the first round" do
+        let(:round) { rounds(:bidding_round) }
 
+        before do
+          submit_bids(round, 1)
+          submit_passes(round, 1, 1)
+          find_next_bidder.call
+        end
+
+        it "returns the correct bidder" do
+          expect(find_next_bidder.next_bidder).to eq(players(:bidder3))
+        end
+      end
     end
 
-    context "when it is the seventh round" do
+    context "when there are multiple bids & multiple passes" do
+      context "when it is the first round" do
+        let(:round) { rounds(:bidding_round) }
 
+        before do
+          submit_bids(round, 1)      # player 1 bids
+          submit_bids(round, 1, 1)   # player 2 bids
+          submit_passes(round, 1, 2) # player 3 passes
+          submit_bids(round, 1, 3)   # player 4 bids
+          submit_bids(round, 1, 4)   # player 1 bids
+          submit_passes(round, 1, 5) # player 2 passes
+          find_next_bidder.call
+        end
+
+        it "returns the correct bidder" do
+          expect(find_next_bidder.next_bidder).to eq(players(:bidder4))
+        end
+      end
+    end
+
+    context "when all players have passed" do
+      let(:round) { rounds(:bidding_round) }
+
+      before do
+        submit_bids(round, 1)      # player 1 bids
+        submit_bids(round, 1, 1)   # player 2 bids
+        submit_passes(round, 1, 2) # player 3 passes
+        submit_bids(round, 1, 3)   # player 4 bids
+        submit_bids(round, 1, 4)   # player 1 bids
+        submit_passes(round, 1, 5) # player 2 passes
+        submit_passes(round, 1, 7) # player 4 passes
+        submit_passes(round, 1, 8) # player 1 passes
+        find_next_bidder.call
+      end
+
+      it "returns nil" do
+        expect(find_next_bidder.next_bidder).to be_nil
+      end
+
+      it "sets a 'bidding is over' error" do
+        expect(find_next_bidder.errors).to include("bidding is over")
+      end
     end
   end
 
-  def submit_bids(number_of_bids)
+  def submit_bids(round, number_of_bids, previous_bids = 0)
     number_of_bids.times do |n|
-      round.bids.create!(player: players("bidder#{(n % game.players.count) + 1}"),
-                         number_of_tricks: 6 + n,
+      round.bids.create!(player: players("bidder#{((n + previous_bids) % game.players.count) + 1}"),
+                         number_of_tricks: 6 + n + previous_bids,
                          suit: rand(0..(Bid.suits.length - 1)))
     end
   end
 
-  def submit_passes(number_of_passes)
+  def submit_passes(round, number_of_passes, previous_bids = 0)
     number_of_passes.times do |n|
-      round.bids.create!(player: players("bidder#{(n % game.players.count) + 1}"),
+      round.bids.create!(player: players("bidder#{((n + previous_bids) % game.players.count) + 1}"),
                          number_of_tricks: 0,
                          suit: 0)
     end
