b902c8c558636fc53f7d5898146d5c4acc82888a
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-05 15:29:51 +1200

    Comprehensively test the play_card service

--- spec/services/play_card_spec.rb
+++ spec/services/play_card_spec.rb
@@ -2,30 +2,75 @@ require "rails_helper"
 
 RSpec.describe PlayCard, type: :service do
   fixtures :all
-  let(:game)  { games(:playing_game) }
-  let(:round) { rounds(:playing_round) }
+  let(:game)      { games(:playing_game) }
+  let(:round)     { rounds(:playing_round) }
   # TODO: we're assuming (for now) that player 2 won the bidding:
-  let(:player) { players(:player2) }
-  let(:card)   { cards(:jack_of_hearts) }
+  let(:player)    { players(:player2) }
+  let(:card)      { cards(:jack_of_hearts) }
+  let(:play_card) { PlayCard.new(round, player, card) }
 
   describe "#call" do
-    before { PlayCard.new(round, player, card).call }
-
     context "when it is the first trick" do
-      context "when the bid-winner attempts to play a card" do
-        it "plays the card" do
-          expect(card.trick).to eq(round.tricks.first)
+      context "when no cards have been played yet" do
+        before { play_card.call }
+
+        context "when the bid-winner attempts to play a card" do
+          it "plays the card" do
+            expect(card.trick).to eq(round.tricks.first)
+          end
+
+          it "doesn't set any errors" do
+            expect(play_card.errors).to be_empty
+          end
+        end
+
+        context "when a non-winning bidder attempts to play a card" do
+          let(:player) { players(:player1) }
+          let(:card)   { cards(:king_of_hearts) }
+
+          it "doesn't play the card" do
+            expect(card.trick).to be_nil
+          end
+
+          it "sets a 'not your turn' error" do
+            expect(play_card.errors).to include("it's not your turn to play")
+          end
         end
       end
 
-      context "when a non-winning bidder attempts to play a card" do
-        let(:player) { players(:player1) }
-        let(:card)   { cards(:king_of_hearts) }
+      context "when cards have been played" do
+        before do
+          PlayCard.new(round, players(:player2), cards(:nine_of_clubs)).call
+          PlayCard.new(round, players(:player3), cards(:seven_of_clubs)).call
 
-        it "doesn't play the card" do
-          expect(card.trick).to be_nil
+          play_card.call
         end
-        # TODO validate error message
+
+        context "when the correct player attempts to play" do
+          let(:player) { players(:player4) }
+          let(:card)   { cards(:five_of_clubs) }
+
+          it "plays the card" do
+            expect(card.trick).to eq(round.tricks.first)
+          end
+
+          it "doesn't set any errors" do
+            expect(play_card.errors).to be_empty
+          end
+        end
+
+        context "when an incorrect player attempts to play" do
+          let(:player) { players(:player1) }
+          let(:card)   { cards(:queen_of_clubs) }
+
+          it "doesn't play the card" do
+            expect(card.trick).to be_nil
+          end
+
+          it "doesn't set any errors" do
+            expect(play_card.errors).to include("it's not your turn to play")
+          end
+        end
       end
     end
 
@@ -37,31 +82,73 @@ RSpec.describe PlayCard, type: :service do
         PlayCard.new(round, players(:player1), cards(:king_of_hearts)).call
       end
 
-      context "when the last trick winner attempts to play a card" do
-        let(:player) { players(:player1) }
-        let(:card)   { cards(:king_of_diamonds) }
+      context "when no cards have been played into this trick" do
+        context "when the last trick winner attempts to play a card" do
+          let(:player) { players(:player1) }
+          let(:card)   { cards(:king_of_diamonds) }
 
-        before { PlayCard.new(round, player, card).call }
+          before do
+            play_card.call
+          end
 
-        it "plays the card" do
-          expect(card.trick).to eq(round.tricks.last)
+          it "plays the card" do
+            expect(card.trick).to eq(round.tricks.last)
+          end
+
+          it "doesn't set any errors" do
+            expect(play_card.errors).to be_empty
+          end
+        end
+
+        context "when an incorrect player attempts to play a card" do
+          let(:player)    { players(:player2) }
+          let(:card)      { cards(:jack_of_clubs)}
+
+          before do
+            play_card.call
+          end
+
+          it "doesn't play the card" do
+            expect(card.trick).to be_nil
+          end
+
+          it "sets the error to 'not your turn'" do
+            expect(play_card.errors).to include("it's not your turn to play")
+          end
         end
       end
 
-      context "when anyone but the last trick winner attempts to play a card" do
-        let(:player)    { players(:player2) }
-        let(:card)      { cards(:jack_of_hearts)}
-        let(:play_card) { PlayCard.new(round, player, card) }
+      context "when cards have been played into this trick" do
+        before do
+          PlayCard.new(round, players(:player1), cards(:ace_of_diamonds)).call
 
-        before          { play_card.call }
-
-        it "doesn't play the card" do
-          expect(card.trick).to be_nil
+          play_card.call
         end
 
-        # TODO validate error message
-        it "sets the error to 'not your turn'" do
-          expect(play_card.errors).to include("it's not your turn to play")
+        context "when the correct player attempts to play a card" do
+          let(:player) { players(:player2) }
+          let(:card)   { cards(:ten_of_diamonds) }
+
+          it "plays the card" do
+            expect(card.trick).to eq(round.tricks.last)
+          end
+
+          it "doesn't set any errors" do
+            expect(play_card.errors).to be_empty
+          end
+        end
+
+        context "when an incorrect player attempts to play a card" do
+          let(:player) { players(:player3) }
+          let(:card)   { cards(:nine_of_diamonds) }
+
+          it "doesn't play the card" do
+            expect(card.trick).to be_nil
+          end
+
+          it "sets the error to 'not your turn'" do
+            expect(play_card.errors).to include("it's not your turn to play")
+          end
         end
       end
     end
