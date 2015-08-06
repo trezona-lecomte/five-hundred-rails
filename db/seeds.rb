3ec9aae610392bc1e58e8a346dde2c20eafd5e99
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-06 15:06:57 +1200

    Add bid submission to game 2 in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,17 @@ deck = BuildDeck.new.call
 DealCards.new(game2, round, deck).call
 
 # bidding:
-#binding.pry
-#SubmitBid.new(round, p1, number_of_tricks, suit)
+SubmitBid.new(round, p1, 6, 0).call # p1 bids 6 spades
+SubmitBid.new(round, p2, 6, 1).call # p2 bids 6 clubs
+SubmitBid.new(round, p3, 7, 0).call # p3 bids 7 clubs
+SubmitBid.new(round, p4, 7, 1).call # p4 bids 7 spades
+SubmitBid.new(round, p1, 0, 0).call # p1 passes
+SubmitBid.new(round, p2, 0, 0).call # p2 passes
+SubmitBid.new(round, p3, 0, 0).call # p3 passes
+SubmitBid.new(round, p4, 0, 0).call # p4 passes & wins the bidding
 
-PlayCard.new(round, p1, p1.cards.sample).call
-PlayCard.new(round, p2, p2.cards.sample).call
-PlayCard.new(round, p3, p3.cards.sample).call
-PlayCard.new(round, p4, p4.cards.sample).call
+# playing:
+#PlayCard.new(round, p1, p1.cards.sample).call
+#PlayCard.new(round, p2, p2.cards.sample).call
+#PlayCard.new(round, p3, p3.cards.sample).call
+#PlayCard.new(round, p4, p4.cards.sample).call
