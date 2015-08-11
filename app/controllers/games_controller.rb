2317b43f3395234428b1058290d76fc17325e772
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-11 22:06:53 +1200

    Refine creation of games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,32 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :update]
+  skip_before_action :authenticate_user_from_token!, only: [:index]
+  # TODO: handle unauthenticated users viewing games (no current_user)
 
   def index
     @games = Game.all
+
+    render json: @games
   end
 
   def show
-    # method left blank - before_action handles setting @game
+    render json: @game
   end
 
   def create
-    game = Game.new
+    @game = Game.new
 
-    if game.save
-      render json: game, status: :created, location: game
+    join_game = JoinGame.new(@game, current_user)
+    join_game.call
+
+    if join_game.errors.present?
+      render json: { errors: join_game.errors }, status: :unprocessable_entity
     else
-      render json: game.errors, status: :unprocessable_entity
+      if @game.save
+        render json: @game, status: :created, location: @game
+      else
+        render json: @game.errors, status: :unprocessable_entity
+      end
     end
   end
 
