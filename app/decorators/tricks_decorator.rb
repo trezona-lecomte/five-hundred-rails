1a6f8c9ec4f8fac35a06f68cef0a5ec7c2731ce7
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-13 10:18:42 +1200

    Add last_played_card to tricks (back-end only so far)

--- app/decorators/tricks_decorator.rb
+++ app/decorators/tricks_decorator.rb
@@ -1,4 +1,8 @@
 class TricksDecorator < SimpleDelegator
+  def last_played_card
+    cards.max_by { |card| card.position_in_trick }
+  end
+
   def winning_card
     # TODO need to account for suit, trumps etc
     cards.max_by { |card| card.rank }
