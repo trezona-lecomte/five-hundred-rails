99de5c6114728ed112ef70c7928267215d7eb818
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-13 10:20:35 +1200

    Tidy routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
   match "*all", to: "application#preflight", via: [:options]
 
   devise_for :users, only: [] # disable devise routes
-  resources  :users, only: [:create, :show]
+  resources  :users, only: [:create]
   resource   :login, only: [:create], controller: :sessions
 
-  resources :games, only: [:index, :show, :create, :update], shallow: true do
-    resources :players, only: [:index, :show, :create]
-    resources :rounds, only: [:show] do
-      resources :bids, only: [:index, :create]
+  resources :games,      only: [:index, :show, :create], shallow: true do
+    resources :players,  only: [:index, :show, :create]
+    resources :rounds,   only: [:index, :show, :create, :update] do
+      resources :bids,   only: [:create]
       resources :tricks, only: [:show]
     end
   end
-
 
   match "*all", to: "application#index", via: [:get]
 end
