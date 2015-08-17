3eb41c972f1769874d24960e541bc8b2b3ac0eb2
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-17 14:10:49 +1200

    Make winning & last_played scopes on card

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,4 +10,9 @@ class Card < ActiveRecord::Base
   validates :round, :rank, :suit, presence: true
   validates :round,               uniqueness: { scope: [:rank, :suit] }
   validates :number_in_trick,     uniqueness: { scope: [:trick], allow_nil: true }
+
+  scope :highest_to_lowest, -> { order(suit: :desc, rank: :desc) }
+  scope :winning,           -> { highest_to_lowest.first }
+  scope :newest_to_oldest,  -> { order(number_in_trick: :desc) }
+  scope :last_played,       -> { newest_to_oldest.first }
 end
