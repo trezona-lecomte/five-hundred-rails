147f188868a4ece3ee1aa244f07494f17e6d4ca1
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-19 15:32:58 +1200

    More TODOs from code review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :validatable
 
-  has_many :players
+  has_many :players # TODO dependent destroy
 
-  validates :username, presence: true
+  validates :username, presence: true # TODO not null in db.
 
+  # Do this in before - after create/save callbacks are icky.
   after_create :update_access_token!
 
   private
@@ -12,6 +13,6 @@ class User < ActiveRecord::Base
   # TODO: firstly user 'rails token auth', secondly use SecureRandom.uuid
   def update_access_token!
     self.access_token = "#{self.id}:#{Devise.friendly_token}"
-    save
+    save # TODO should have bang
   end
 end
