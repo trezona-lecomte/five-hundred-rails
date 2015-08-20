3a01361f5fa74bd89f04c0eafd03461bf7137a42
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-20 14:28:10 +1200

    Create modules for rank & suit constants

---
+++ app/modules/suits.rb
@@ -0,0 +1,9 @@
+module Suits
+  SPADES   = "spades"
+  CLUBS    = "clubs"
+  DIAMONDS = "diamonds"
+  HEARTS   = "hearts"
+  NO_SUIT  = "no_suit"
+
+  ALL_SUITS = [SPADES, CLUBS, DIAMONDS, HEARTS, NO_SUIT]
+end
