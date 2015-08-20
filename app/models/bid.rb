50394d00392128ab7be44517c3b48d9258f12095
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-20 15:25:00 +1200

    Implement a pass_params class method on bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -16,7 +16,7 @@ class Bid < ActiveRecord::Base
   scope :passes,          -> { where(number_of_tricks: 0) }
   scope :in_ranked_order, -> { order(number_of_tricks: :desc, suit: :desc) }
 
-  # def self.pass_bid
-  #   self.new(suit: Suits::NO_SUIT, )
-  # end
+  def self.pass_params
+    { number_of_tricks: PASS_TRICKS, suit: NO_SUIT }
+  end
 end
