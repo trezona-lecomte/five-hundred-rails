d03fa55cd4ec902f02c956624c0e9d94ea7d3ea7
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-21 10:43:02 +1200

    Add a game preview serializers for games#index

---
+++ app/serializers/game_preview_serializer.rb
@@ -0,0 +1,17 @@
+class GamePreviewSerializer < ActiveModel::Serializer
+  attributes :id,
+             :path,
+             :stage
+
+  def path
+    game_path(object)
+  end
+
+  def stage
+    if object.finished?
+      "finished"
+    else
+      "in progress"
+    end
+  end
+end
