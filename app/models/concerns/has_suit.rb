e628d7c982a932b64afc4111d8fc24f5825dd721
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-21 14:52:36 +1200

    Delete has_suit - replaced by explicit definitions

--- app/models/concerns/has_suit.rb
+++
@@ -1,8 +0,0 @@
-module HasSuit
-  # TODO Store suits in a constant and refer to it when constructing the attr in bid and card.
-  extend ActiveSupport::Concern
-
-  included do
-    enum suit: %w(spades clubs diamonds hearts no_suit)
-  end
-end
