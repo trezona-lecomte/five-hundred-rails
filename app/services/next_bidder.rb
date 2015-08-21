ea90864c087689de263bc5a16aaf16c5f6b42106
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-21 13:33:16 +1200

    Tidy up next_bidder service

--- app/services/next_bidder.rb
+++ app/services/next_bidder.rb
@@ -21,23 +21,43 @@ class NextBidder
   private
 
   def set_next_bidder
+    if @round.bids.none?
+      set_bidder_for_first_bid
+    else
+      set_bidder_for_subsequent_bid
+    end
+  end
+
+  def set_bidder_for_first_bid
+    next_bidder_number = @round.number_in_game % @players.length
+
+    if next_bidder_number == 0
+      next_bidder_number = @players.length
+    end
+
+    @next_bidder = @players.detect do |player|
+      player.number_in_game == next_bidder_number
+    end
+  end
+
+  def set_bidder_for_subsequent_bid
     passed_players_offset = 0
 
     until @next_bidder || passed_players_offset > 3
       incremented_index = next_bidder_index(passed_players_offset)
 
-      bidder = fetch_next_bidder(incremented_index)
+      @next_bidder = fetch_next_bidder(incremented_index)
 
-      unless bidder_has_already_passed?(bidder)
-        @next_bidder = bidder
+      if bidder_has_already_passed?
+        @next_bidder = nil
       end
 
       passed_players_offset += 1
     end
   end
 
-  def bidder_has_already_passed?(bidder)
-    @round.bids.passes.any? { |pass| pass.player == bidder }
+  def bidder_has_already_passed?
+    @round.bids.passes.any? { |pass| pass.player == @next_bidder }
   end
 
   def next_bidder_index(offset)
