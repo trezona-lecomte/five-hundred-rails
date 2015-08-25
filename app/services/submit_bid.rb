c1816b99194ec14e8c9ae65b36f11c3b5c569f2d
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-26 10:09:09 +1200

    Move non turn-related validations from bidder_turn_validator to submit_bid

--- app/services/submit_bid.rb
+++ app/services/submit_bid.rb
@@ -1,15 +1,18 @@
 class SubmitBid
   include ActiveModel::Validations
 
-  attr_reader :round, :player, :number_of_tricks, :suit
+  attr_reader :round, :player, :number_of_tricks, :suit, :highest_bid
 
-  validates_with BidSubmissionValidator
+  validate :bidding_is_open
+  validate :bid_is_high_enough
+  validates_with BidderTurnValidator
 
   def initialize(round:, player:, number_of_tricks:, suit:)
     @round = round
     @player = player
     @number_of_tricks = number_of_tricks
     @suit = suit
+    @highest_bid = round.bids.last
   end
 
   def call
@@ -19,6 +22,43 @@ class SubmitBid
   end
 
   private
+
+  def bidding_is_open
+    errors.add(:base, "bidding for this round has finished") unless round.in_bidding_stage?
+  end
+
+  def bid_is_high_enough
+    errors.add(:base, "your bid isn't high enough") unless bid_is_higher_than_previous_or_is_pass?
+  end
+
+  # TODO: how can I move this resp. onto bid..
+  # TODO: instantiate a bid in submit_bid initializer, then use comparison of bids to do the below logic.
+  def bid_is_higher_than_previous_or_is_pass?
+    bid_is_a_pass? ||
+    bid_is_first_in_round? ||
+    bid_number_of_tricks_are_higher_than_previous? ||
+    (bid_number_of_tricks_are_equal_to_previous? && bid_suit_is_higher_than_previous?)
+  end
+
+  def bid_is_first_in_round?
+    !highest_bid
+  end
+
+  def bid_is_a_pass?
+    number_of_tricks == Bid::PASS_TRICKS
+  end
+
+  def bid_number_of_tricks_are_higher_than_previous?
+    number_of_tricks > highest_bid.number_of_tricks
+  end
+
+  def bid_number_of_tricks_are_equal_to_previous?
+    number_of_tricks == highest_bid.number_of_tricks
+  end
+
+  def bid_suit_is_higher_than_previous?
+    Bid.suits[suit] > Bid.suits[highest_bid.suit]
+  end
 
   def submit_bid!
     begin
--- spec/services/submit_bid_spec.rb
+++ spec/services/submit_bid_spec.rb
@@ -1,3 +1,4 @@
+# TODO: add negative tests for bids having to be higher
 require "rails_helper"
 
 RSpec.describe SubmitBid, type: :service do
@@ -8,7 +9,11 @@ RSpec.describe SubmitBid, type: :service do
   let(:player)           { players(:bidder1) }
   let(:number_of_tricks) { Bid::PASS_TRICKS }
   let(:service_args)     { { round: round, player: player, number_of_tricks: number_of_tricks, suit: suit } }
-  let(:service)          { SubmitBid.new(**service_args) }
+
+  subject(:service) { SubmitBid.new(**service_args) }
+
+  BIDDING_IS_FINISHED_ERROR = "bidding for this round has finished"
+  BID_NOT_HIGH_ENOUGH_ERROR = "your bid isn't high enough"
 
   describe "#call" do
     before { allow(service).to receive(:valid?).and_return(validity) }
@@ -26,6 +31,111 @@ RSpec.describe SubmitBid, type: :service do
 
       it "doesn't submit the bid" do
         expect { service.call }.to_not change(Bid, :count)
+      end
+    end
+  end
+
+  describe "#bidding_is_open" do
+    context "when the round is not in the bidding stage" do
+      let(:round) { rounds(:playing_round) }
+
+      before { service.valid? }
+
+      it { is_expected.to be_invalid }
+
+      it "has the correct 'round has finished' error" do
+        expect(service.errors[:base]).to include(BIDDING_IS_FINISHED_ERROR)
+      end
+    end
+  end
+
+  describe "#bid_is_high_enough" do
+    context "when the bid is the first of the round" do
+        let(:number_of_tricks) { (Bid::MIN_TRICKS..Bid::MAX_TRICKS).to_a.sample }
+
+        it { is_expected.to be_valid }
+    end
+
+    context "when the bid is high enough" do
+      let(:previous_tricks_bid) { (Bid::MIN_TRICKS..(Bid::MAX_TRICKS - 1)).to_a.sample }
+      let(:player)              { players(:bidder2) }
+
+      before do
+        SubmitBid.new(round: round,
+                      player: players(:bidder1),
+                      suit: Suits::HEARTS,
+                      number_of_tricks: previous_tricks_bid).call
+      end
+
+      context "when the bid is a pass" do
+        let(:number_of_tricks) { Bid::PASS_TRICKS }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "when the bid number of tricks are higher than the previous bid" do
+        let(:suit)             { Suits::HEARTS }
+        let(:number_of_tricks) { ((previous_tricks_bid + 1)..Bid::MAX_TRICKS).to_a.sample }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "when the bid number of tricks are equal but the suit is higher than the previous bid" do
+        let(:suit)             { Suits::NO_SUIT }
+        let(:number_of_tricks) { previous_tricks_bid }
+
+        it { is_expected.to be_valid }
+      end
+    end
+
+    context "when the bid isn't high enough" do
+      let(:previous_tricks_bid) { (Bid::MIN_TRICKS..(Bid::MAX_TRICKS - 1)).to_a.sample }
+      let(:player)              { players(:bidder2) }
+
+      before do
+        SubmitBid.new(round: round,
+                      player: players(:bidder1),
+                      suit: Suits::HEARTS,
+                      number_of_tricks: previous_tricks_bid).call
+      end
+
+      context "when the number of tricks aren't higher than the previous bid" do
+        let(:suit)             { Suits::NO_SUIT }
+        let(:number_of_tricks) { previous_tricks_bid - 1 }
+
+        before { service.valid? }
+
+        it { is_expected.to be_invalid }
+
+        it "has the correct 'bid not high enough' error" do
+          expect(service.errors[:base]).to include(BID_NOT_HIGH_ENOUGH_ERROR)
+        end
+      end
+
+      context "when the number of tricks and the suit are the same as the previous bid" do
+        let(:suit)             { Suits::HEARTS }
+        let(:number_of_tricks) { previous_tricks_bid }
+
+        before { service.valid? }
+
+        it { is_expected.to be_invalid }
+
+        it "has the correct 'bid not high enough' error" do
+          expect(service.errors[:base]).to include(BID_NOT_HIGH_ENOUGH_ERROR)
+        end
+      end
+
+      context "when the number of tricks is the same and the suit is lower than the previous bid" do
+        let(:suit)             { Suits::DIAMONDS }
+        let(:number_of_tricks) { previous_tricks_bid }
+
+        before { service.valid? }
+
+        it { is_expected.to be_invalid }
+
+        it "has the correct 'bid not high enough' error" do
+          expect(service.errors[:base]).to include(BID_NOT_HIGH_ENOUGH_ERROR)
+        end
       end
     end
   end
