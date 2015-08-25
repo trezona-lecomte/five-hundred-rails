248bf02b5325f7574388fcabc054547ee1cf71ca
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-25 14:50:44 +1200

    Generate AR bids rather than passing round params

---
+++ app/services/generate_available_bids.rb
@@ -0,0 +1,61 @@
+# TODO: generate potential bids?
+# NOTE: passing around params requires outside world to have too much knowledge of the internals -
+# passing around bid AR objects would be cleaner.
+# TODO: can use pass scope for construction of pass bids after ^^ is done.
+class GenerateAvailableBids
+  include ActiveModel::Validations
+
+  attr_reader :round, :available_bids
+
+  validate :round_can_be_bid_on
+
+  def initialize(round)
+    @round = round
+    @available_bids = []
+  end
+
+  def call
+    round.with_lock do
+      valid? && generate_available_bids
+    end
+  end
+
+  private
+
+  def round_can_be_bid_on
+    errors.add(:base, "this round isn't in the bidding stage") if !round.in_bidding_stage?
+  end
+
+  def generate_available_bids
+    if any_non_pass_bids_made_yet?
+      @available_bids = [Bid.passes.new] + bids_above_current_highest_bid
+    else
+      @available_bids = all_possible_bids
+    end
+  end
+
+  def any_non_pass_bids_made_yet?
+    round.bids.non_passes.any?
+  end
+
+  def all_possible_bids
+    [Bid.passes.new] + all_possible_non_pass_bids
+  end
+
+  def all_possible_non_pass_bids
+    (Bid::MIN_TRICKS..Bid::MAX_TRICKS).to_a.product(Bid.suits.keys).map do |tricks, suit|
+      Bid.new(number_of_tricks: tricks, suit: suit)
+    end
+  end
+
+  def bids_above_current_highest_bid
+    highest_bid    = round.highest_bid
+    highest_tricks = highest_bid.number_of_tricks
+    highest_suit   = highest_bid.suit
+
+    all_possible_non_pass_bids.select do |bid|
+      (bid.number_of_tricks > highest_tricks) ||
+      (bid.number_of_tricks == highest_tricks && Bid.suits[bid.suit] > Bid.suits[highest_suit])
+    end
+  end
+end
---
+++ spec/services/generate_available_bids_spec.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+RSpec.describe GenerateAvailableBids, type: :service do
+  fixtures :all
+
+  describe "#call" do
+    let(:round)       { rounds(:bidding_round) }
+    let(:service)     { GenerateAvailableBids.new(round) }
+    let(:call_result) { service.call }
+
+    context "when the round is in the bidding stage" do
+      before { service.call }
+
+      let(:bids)        { service.available_bids }
+
+      context "when no bids have been made" do
+        it "returns true" do
+          expect(call_result).to be_truthy
+        end
+
+        it "generates a full list of all possible bids" do
+          expect(bids.count).to eq(26)
+        end
+      end
+
+      context "when bids have been made" do
+        let(:highest_bid)   { Bid.create!(round: round,
+                                          suit: Suits::NO_SUIT,
+                                          number_of_tricks: Bid::MAX_TRICKS,
+                                          player: players(:bidder1)) }
+        let(:excluded_bids) { excluded_bids_for(highest_bid) }
+
+        before do
+          allow(service).to receive(:any_non_pass_bids_made_yet?).and_return(true)
+          allow(round).to receive(:highest_bid).and_return(highest_bid)
+          service.call
+        end
+
+        it "returns true" do
+          expect(call_result).to be_truthy
+        end
+
+        it "generates a list of all bids higher than the last submitted bid & and a pass" do
+          expect(service.available_bids.count).to eq(1)
+        end
+      end
+    end
+
+    context "when the round is in the playing stage" do
+      let(:round) { rounds(:playing_round) }
+
+      it "returns false" do
+        expect(call_result).to be false
+      end
+    end
+
+    context "when the round is finished" do
+      let(:round) { rounds(:finished_round) }
+
+      it "returns false" do
+        expect(call_result).to be false
+      end
+    end
+  end
+end
