f2c5b877a16bb974d5834d0ae6db205a768d494d
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2015-08-26 17:38:51 +1200

    CR: functionalify build_deck

--- app/services/build_deck.rb
+++ app/services/build_deck.rb
@@ -1,9 +1,9 @@
 class BuildDeck
   attr_reader :deck
 
-  def initialize
-    @deck = []
-  end
+  # def initialize
+  #   @deck = []
+  # end
 
   def call
     standard_deck.shuffle!
@@ -12,27 +12,30 @@ class BuildDeck
   private
 
   def standard_deck
-    standard_ranks.product(standard_suits).each do |rank, suit|
-      @deck << Card.new(rank: rank, suit: suit)
-    end
+    standard_ranks.product(standard_suits).map do |rank, suit|
+      Card.new(rank: rank, suit: suit)
+    end + red_fours + [joker]
 
-    @deck += red_fours
-    @deck << joker
+    # @deck += red_fours
+    # @deck << joker
   end
 
+  # TODO should be using enum values for this filter (build an array of wanted cards & check inclusion)
   def standard_ranks
     Card.ranks.select { |_, rank| rank.between?(5, 17) && !rank.between?(11, 13) }
       .keys
   end
 
   def standard_suits
-    Card.suits.select { |suit, _| suit != Suits::NO_SUIT }
-      .keys
+    Card.suits.keys - [Suits::NO_SUIT]
   end
 
+  # TODO again (next two methods), use enum values
   def red_fours
-    [Card.new(rank: "4", suit: Suits::HEARTS),
-     Card.new(rank: "4", suit: Suits::DIAMONDS)]
+    [
+      Card.new(rank: "4", suit: Suits::HEARTS),
+      Card.new(rank: "4", suit: Suits::DIAMONDS)
+    ]
   end
 
   def joker
